b2f95400922b309ee8bbbc4950f36025dedc5274
Mahnooranwar191 <mahnnooranwar191@gmail.com>
2026-03-08 01:26:10 +0500

    8/march!

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -10,78 +10,7 @@ const Index = () => {
 <Navbar/>
 
 
- <div className='flex gap-10 p-8  flex-col lg:flex-row '>
-    <div className='w-full lg:w-5/12   lg:px-10 lg:py-30  '>
-   
-
-  <span className='text-[#FFFFFF59] text-sm font-semibold uppercase  mb-2'>
-    Hello, i’m
-  </span>
-
-  <h1 className='text-[#C9F31D] font-bold tracking-tight text-6xl leading-none mb-2'>
-    Md Hassan
-  </h1>
-  
-
-  <h2 className='text-white text-4xl font-bold mb-4'>
-    Digital Marketer
-  </h2>
-  
-
-  <p className='text-[#8B8B8B] text-lg leading-relaxed max-w-md'>
-    We denounce with righteous indignation dislike demoralized by the charms of pleasure.
-  </p>
-    <p className='text-[#8B8B8B] text-lg leading-relaxed max-w-md'>
-    We denounce with righteous indignation dislike demoralized by the charms of pleasure.
-  </p>
-  
-
-<button className="group relative inline-block px-10 py-4 overflow-hidden rounded-md border border-[#C9F31D] font-bold text-black bg-[#C9F31D] transition-all duration-300 ease-in-out hover:text-white mt-5 hover:shadow-lg  shadow-[#070707]">
-  
-
-  <span className="absolute left-1/2 top-full h-[180%] w-[140%] -translate-x-1/2 rounded-[50%] bg-black/5 transition-all duration-500 ease-[cubic-bezier(0.55,0,0.1,1)] group-hover:-top-[35%] group-hover:scale-x-[0.8] group-hover:scale-y-[1.3] z-0"></span>
-
-  <span className="absolute left-[55%] top-[180%] h-[190%] w-[160%] -translate-x-1/2 rounded-[50%] bg-black transition-all duration-500 ease-[cubic-bezier(0.55,0,0.1,1)] group-hover:-top-[45%] group-hover:scale-x-[0.8] group-hover:scale-y-[1.3] z-0"></span>
-
-
-  <span className="relative z-10">Download CV</span>
-</button>
-<button className="group relative ml-2 inline-block px-10 py-4 overflow-hidden rounded-md border hover:border-[#C9F31D] font-bold text-black bg-white transition-all duration-300 ease-in-out hover:text-white mt-5 hover:shadow-lg  shadow-[#070707]">
-  
-
-  <span className="absolute left-1/2 top-full h-[180%] w-[140%] -translate-x-1/2 rounded-[50%] bg-black/5 transition-all duration-500 ease-[cubic-bezier(0.55,0,0.1,1)] group-hover:-top-[35%] group-hover:scale-x-[0.8] group-hover:scale-y-[1.3] z-0"></span>
-
-  <span className="absolute left-[55%] top-[180%] h-[190%] w-[160%] -translate-x-1/2 rounded-[50%] bg-black transition-all duration-500 ease-[cubic-bezier(0.55,0,0.1,1)] group-hover:-top-[45%] group-hover:scale-x-[0.8] group-hover:scale-y-[1.3] z-0"></span>
-
-
-  <span className="relative z-10">Hire me</span>
-</button>
-    </div>
-  <div className='flex justify-evenly flex-col lg:flex-row gap-15 mt-5 py-10 lg:w-6/12 '>
-    <div className=''>
-        <img src="https://wp.webtend.net/noxfolio/wp-content/uploads/2023/11/me.png"  className='border-5 border-[#C9F31D] rounded-full w-full' alt="" />
-    </div>
-<div className='lg:w-4/12 w-full  '>
-<div className=' h-full'>
-    <ul className='bg-[#070707] px-5 py-4 mt-10  rounded-2xl'>
-        <li className='my-5 border-b-2  border-gray-500 p-2'>
-            <h3 className='text-[#C9F31D] text-4xl font-bold mb-2 '>13+</h3>
-            <p className='text-[#8B8B8B]'>Years of experience</p>
-        </li>
-        <li className='my-5 border-b-2  border-gray-500 p-3'>
-            <h3 className='text-[#C9F31D] text-4xl font-bold mb-2 '>13+</h3>
-            <p className='text-[#8B8B8B]'>Years of experience</p>
-        </li>
-        <li className='my-5 border-b-2  border-gray-500 p-3'>
-            <h3 className='text-[#C9F31D] text-4xl font-bold mb-2 '>13+</h3>
-            <p className='text-[#8B8B8B]'>Years of experience</p>
-        </li>
-      
-    </ul>
-</div>
-</div>
-  </div>
-</div> 
+<HeroSection/>
 
 <section className="mt-5 ">
     <div className='container bg-black mx-auto lg:flex-row flex-col-reverse p-10 flex justify-between   rounded-2xl'>
